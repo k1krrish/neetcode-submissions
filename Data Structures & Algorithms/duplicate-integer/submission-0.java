1ad697f26ea6807f42152b82a16e5cf4da550969
Krrish Mantri <krinja.krrish1306@gmail.com>
2026-09-02 22:36:30 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet();
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+
+            }
+            set.add(num);
+        }
+        return false;
+
+       
+        
+
+        
+
+    }
+}
