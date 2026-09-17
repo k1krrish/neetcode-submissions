c6c5302c59b2fa5a1b034fe9ae9b466118cc4d4f
Krrish Mantri <krinja.krrish1306@gmail.com>
2026-09-17 23:48:00 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length!=t.length){
+            return false;
+        }
+        s=s.split("").sort();
+        t=t.split("").sort();
+        for(let i=0;i<s.length;i++){
+            if(s[i]!==t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+        
+}
